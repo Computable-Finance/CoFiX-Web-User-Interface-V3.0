1e3536f3d09e3e0123afca6b7f514524f3ea9e6c
tunogya <tunogya@qq.com>
2021-11-25 15:33:01 +0800

    delete unused address

--- src/libs/web3/constants/contracts.ts
+++ src/libs/web3/constants/contracts.ts
@@ -1,8 +1,4 @@
-import { UniswapQuoterProps } from '../api/UniswapQuoter'
-import { CoFiXControllerProps } from '../api/CoFiXController'
-import { CoFiXDAOProps } from '../api/CoFixDAO'
 import { CoFiXRouterProps } from '../api/CoFiXRouter'
-import { CoFiXVaultForStakingProps } from '../api/CoFiXVaultForStaking'
 import { NestPriceFacadeProps } from '../api/NestPriceFacade'
 import { Mainnet, Testnet } from './chains'
 
@@ -13,20 +9,6 @@ export const NestPriceFacade: NestPriceFacadeProps = {
   },
 }
 
-export const UniswapQuoter: UniswapQuoterProps = {
-  addresses: {
-    [Mainnet.chainId]: '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6',
-    [Testnet.chainId]: '0xA3091BbDe2A1517AFf8e38a97A7D755fC36b7Eb9',
-  },
-}
-
-export const CoFiXDAO: CoFiXDAOProps = {
-  addresses: {
-    [Mainnet.chainId]: '0x76D8680e763c611f204c974cf2F6c203d44fd124',
-    [Testnet.chainId]: '0x76D8680e763c611f204c974cf2F6c203d44fd124',
-  },
-}
-
 export const CoFiXRouter: CoFiXRouterProps = {
   addresses: {
     [Mainnet.chainId]: '0x4A448cBb12e449D7031f36C8122eCE6dDdf9cc84',
@@ -34,16 +16,3 @@ export const CoFiXRouter: CoFiXRouterProps = {
   },
 }
 
-export const CoFiXVaultForStaking: CoFiXVaultForStakingProps = {
-  addresses: {
-    [Mainnet.chainId]: '0x7Bd4546DEdB397a0f0D7593A7Fa7f2Ceb3ff32E6',
-    [Testnet.chainId]: '0x11839c81beBBC82686b0052Cb6F03E9Ae58A9704',
-  },
-}
-
-export const CoFiXController: CoFiXControllerProps = {
-  addresses: {
-    [Mainnet.chainId]: '0x8eFFbf9CA7dB20481cE9C25EA4B410b3B835D70E',
-    [Testnet.chainId]: '0x59c2EAF8FC22C10C2EB79Be3c23c2916BD0ec81e',
-  },
-}
--- src/libs/web3/constants/tokens.tsx
+++ src/libs/web3/constants/tokens.tsx
@@ -1,9 +1,6 @@
 import {
   TokenNEST,
-  TokenPETH, TokenPUSD,
-  TokenUSDC,
   TokenUSDT,
-  TokenWETH,
 } from 'src/components/Icon'
 
 import { TokenProps } from '../api/Token'
@@ -18,23 +15,6 @@ export const USDT: TokenProps = {
   },
 }
 
-export const PETH: TokenProps = {
-  symbol: 'PETH',
-  Icon: TokenPETH,
-  addresses: {
-    [Mainnet.chainId]: '0xc39dC1385a44fBB895991580EA55FC10e7451cB3',
-    [Testnet.chainId]: '0xc39dC1385a44fBB895991580EA55FC10e7451cB3',
-  },
-}
-
-export const USDC: TokenProps = {
-  symbol: 'USDC',
-  Icon: TokenUSDC,
-  addresses: {
-    [Mainnet.chainId]: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    [Testnet.chainId]: '0xB64825a6bA80d65886b5123f5170ddffc935D9DE',
-  },
-}
 
 export const NEST: TokenProps = {
   symbol: 'NEST',
@@ -46,24 +26,5 @@ export const NEST: TokenProps = {
   formatPrecision: 2,
 }
 
-export const PUSD: TokenProps = {
-  symbol: 'PUSD',
-  Icon: TokenPUSD,
-  addresses: {
-    [Mainnet.chainId]: '0x3DA5c9aafc6e6D6839E62e2fB65825869019F291',
-    [Testnet.chainId]: '0x3DA5c9aafc6e6D6839E62e2fB65825869019F291',
-  },
-  formatPrecision: 2,
-}
 
-export const WETH: TokenProps = {
-  symbol: 'WETH',
-  Icon: TokenWETH,
-  addresses: {
-    [Mainnet.chainId]: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-    [Testnet.chainId]: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
-  },
-}
-
-
-export const ERC20TokenWhitelist = [USDT, NEST, PUSD]
+export const ERC20TokenWhitelist = [USDT, NEST]
