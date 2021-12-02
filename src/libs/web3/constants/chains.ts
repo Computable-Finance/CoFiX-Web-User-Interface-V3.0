12bb694299ab5a89d17860918bc1a6b2de9d56f7
tunogya <tunogya@qq.com>
2021-12-02 11:43:19 +0800

    delete unsupported chains

--- src/libs/web3/constants/chains.ts
+++ src/libs/web3/constants/chains.ts
@@ -53,24 +53,6 @@ export const Mainnet = {
   infoURL: 'https://ethereum.org',
 }
 
-export const Ropsten = {
-  name: 'Ethereum Testnet Ropsten',
-  chainId: 3,
-  shortName: 'rop',
-  chain: 'ETH',
-  network: 'ropsten',
-  networkId: 3,
-  nativeCurrency: {
-    name: 'Ropsten Ether',
-    symbol: 'ROP',
-    decimals: 18,
-  },
-  rpc: [`https://ropsten.infura.io/v3/${INFURA_API_KEY}`, `wss://ropsten.infura.io/ws/v3/${INFURA_API_KEY}`],
-  faucets: ['https://faucet.ropsten.be?${ADDRESS}'],
-  explorers: [],
-  infoURL: 'https://github.com/ethereum/ropsten',
-}
-
 export const Rinkeby = {
   name: 'Ethereum Testnet Rinkeby',
   chainId: 4,
@@ -89,46 +71,4 @@ export const Rinkeby = {
   infoURL: 'https://www.rinkeby.io',
 }
 
-export const Goerli = {
-  name: 'Ethereum Testnet Görli',
-  chainId: 5,
-  shortName: 'gor',
-  chain: 'ETH',
-  network: 'goerli',
-  networkId: 5,
-  nativeCurrency: {
-    name: 'Görli Ether',
-    symbol: 'GOR',
-    decimals: 18,
-  },
-  rpc: ['https://rpc.goerli.mudit.blog/', 'https://rpc.slock.it/goerli ', 'https://goerli.prylabs.net/'],
-  faucets: ['https://goerli-faucet.slock.it/?address=${ADDRESS}', 'https://faucet.goerli.mudit.blog'],
-  explorers: [],
-  infoURL: 'https://goerli.net/#about',
-}
-
-export const Kovan = {
-  name: 'Ethereum Testnet Kovan',
-  chainId: 42,
-  shortName: 'kov',
-  chain: 'ETH',
-  network: 'kovan',
-  networkId: 42,
-  nativeCurrency: {
-    name: 'Kovan Ether',
-    symbol: 'KOV',
-    decimals: 18,
-  },
-  rpc: [
-    `https://kovan.poa.network`,
-    `http://kovan.poa.network:8545`,
-    `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
-    `wss://kovan.infura.io/ws/v3/${INFURA_API_KEY}`,
-    `ws://kovan.poa.network:8546`,
-  ],
-  faucets: ['https://faucet.kovan.network', 'https://gitter.im/kovan-testnet/faucet'],
-  explorers: [],
-  infoURL: 'https://kovan-testnet.github.io/website',
-}
-
-export const SupportedChains: Array<Chain> = [Mainnet, Ropsten, Rinkeby, Goerli, Kovan]
+export const SupportedChains: Array<Chain> = [Mainnet, Rinkeby]
